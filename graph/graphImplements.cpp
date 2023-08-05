dd08e3447f1c08521d409cf052536575d258e6c3
osama78assi <138003027+osama78assi@users.noreply.github.com>
2023-08-06 02:03:21 +0300

    Create graphImplements.cpp

---
+++ graph/graphImplements.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int graph[][10], int nodes) {
+    int i, j;
+    cout << "\n\n" << setw(4) << " ";
+    for(i = 0; i < nodes; i++)
+        cout << setw(3) << "(" << i+1 << ")";
+    cout << "\n\n";
+    for(i = 0; i < nodes; i++) {
+        cout << setw(3) << "(" << i+1 << ")";
+        for(j = 0; j < nodes; j++) {
+            cout << setw(4) << graph[i][j];
+        }
+        cout << "\n\n";
+    }
+}
+
+int main() {
+    int i, j, v;
+    cout << "Enter How Many Vertexes: ";
+    cin >> v;
+    int graph[10][10];
+    cout << "\n";
+    //Take Input Of Adjacency Of Each Pair Of Vertexes
+    for(i = 0; i < v; i++) {
+        for(j = i; j < v; j++) {
+            if(i!=j) {
+                cout << "Enter 1 If Vertex " << i+1 << " Is Adjacent To " << j+1 << " , Otherwise 0: ";
+                cin >> graph[i][j];
+                graph[j][i] = graph[i][j];
+            }
+            else
+                graph[i][j] = 0;
+        }
+    }
+    print(graph, v);
+    return 0;
+}
