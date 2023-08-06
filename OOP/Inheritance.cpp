123b5309a5ce223fc93fa480cfa475173e94e108
Osama Assi <138003027+osama78assi@users.noreply.github.com>
2023-08-06 18:59:19 +0300

    Create Inheritance.cpp

---
+++ OOP/Inheritance.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class employee {
+private:
+    int salary;
+    string name;
+    string company;
+    int age;
+public:
+    employee(string Name, string Company, int Age) {
+        name = Name;
+        company = Company;
+        age = Age;
+        salary = 5000;
+    }
+
+    virtual void details() {
+        cout << "Name: " << name << "\n";
+        cout << "Work For: " << company << "\n";
+        cout << age << " Years Old\n";
+        cout << "Salary: " << salary << "$\n";
+    }
+};
+// Inherit From Class One With All Public Things
+// Like A Method, Properties...etc
+class developer : public employee {
+private:
+    string work;
+public:
+    // You Can Build It's Own Constructor
+    // Or Inherit The Constructor And Give It
+    // The Needed Parameters
+    developer(string Name, string Company, int Age, string Work)
+        : employee(Name, Company, Age) {
+        work = Work;
+    }
+
+    void details() override {
+        employee::details(); // Call the base class function
+        cout << "Specialty: " << work << "\n";
+    }
+};
+
+int main() {
+    employee emp1("osama", "Google", 32);
+    emp1.details();
+    cout << "\n####\n";
+    developer emp2("moumin", "IBM", 32, "Cyber Security");
+    emp2.details();
+    return 0;
+}
